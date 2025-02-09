cc3e059c1a42a22ec4d9ad4c77af537c61ae82b3
giglaulala <gigla.titina777@gmail.com>
2025-02-09 22:27:45 +0400

    feat: add change in html

--- rs-react-app/src/components/Details.tsx
+++ rs-react-app/src/components/Details.tsx
@@ -37,7 +37,7 @@ const Details: React.FC<DetailsProps> = ({ onClose }) => {
         Close
       </button>
       {isLoading ? (
-        <div>Loading...</div>
+        <div>Loading....</div>
       ) : (
         details && (
           <div>
